4255496c28f38bea91ac459e2a7b4c8382397cd2
Kristina Diaz <kristina.diaz84@gmail.com>
2022-01-10 19:30:52 -0700

    commit tree 9e9577bc4bb3c34ed9e293f0292d0f5a1dd35cfb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    students = if params[:name]
+      Student.all.where(last_name: params[:name].capitalize).to_a
+    else
+      Student.all
+    end    
+      render json: students
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
-  get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students/highest_grade', to: 'students#highest_grade'
+  get '/students/:id', to: 'students#show'
+  get '/students?:key=value', to: 'students#index'
 end
